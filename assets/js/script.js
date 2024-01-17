b540ef8fcf2635f64f9b32dc05a54fd951f94c3f
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-01-17 21:26:28 +0000

    started testing API results.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,41 @@
 $(function () {
 
-    console.log('ding')
-
 const newsAPIKey = "3661478808829817c896e863d91b2c1dd0dba"
-const newsBaseURL = `https://newsdata.io/api/1/news?apikey=${APIKey}`
 
-}
+// Fake user input - this will be linked to the input element on the HTML file later
+let userInput = 'pizza'
+
+// Base URL with language 
+const newsBaseURL = `https://newsdata.io/api/1/news?apikey=pub_${newsAPIKey}&q=${userInput}&language=en`
+
+// Fetch method
+    fetch(newsBaseURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+
+// For loop - to render relevant data from the first 5 results 
+            // for (let i = 0; i < 5; i++) {
+            
+// Article titles, images, descriptions, links
+            console.log(data.results[0].title)
+            let title = data.results[0].title
+            console.log(data.results[0].image_URL)
+            let img = data.results[0].image_URL
+            console.log(data.results[0].description)
+            let description = data.results[0].image_URL
+            console.log(data.results[0].link)
+            let link = data.results[0].link
+
+        $('<h1>').text(title)
+            $('<img>').attr('src', data.results[0].image_URL)
+            $()
+            
+            // }
+
+        })
+
+})
+
