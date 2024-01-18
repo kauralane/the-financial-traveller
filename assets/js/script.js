a16250193f3b7a60ce5ed4c552ce26049416e311
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-01-18 20:38:24 +0000

    amended URL to include user input. Also created input box and search button in HTML, to link in to javascript.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,28 @@
-$(document).ready(function () {
+$(function () {
   const apiKeyEx = "8d6403f7b11515b6add06ccb75af36f5";
   const apiBaseURLEx = "https://api.exchangeratesapi.io/v1/";
-  console.log('test')
-})
 
-$(function () {
 
-const newsAPIKey = "3661478808829817c896e863d91b2c1dd0dba"
 
-// Fake user input - this will be linked to the input element on the HTML file later
-let userInput = 'gb'
 
-// Base URL with language 
-    const newsBaseURL = `https://newsdata.io/api/1/news?apikey=pub_${newsAPIKey}&q=economy&language=en&category=business,politics,crime,technology,domestic&country=${userInput}`
 
-// Fetch method
+
+// NEWS API CODE - LK
+const newsAPIKey = "3661478808829817c896e863d91b2c1dd0dba";
+let userInput = $('#search-input').val().trim();
+
+    $('#search-button').on('click', function () {
+        fetchNews()
+    })
+
+// Fetch function
+function fetchNews() {
+
+    let userInput = $('#search-input').val().trim();
+    
+    // Base URL with language, category, and query parameter 'economy'. Also limits number of articles returned to 5. Doesn't seem to work
+    const newsBaseURL = `https://newsdata.io/api/1/news?apikey=pub_${newsAPIKey}&q=economy%20AND%20${userInput}&language=en&category=business,politics,crime,technology,domestic&size=5`
+
     fetch(newsBaseURL)
         .then(function (response) {
             return response.json();
@@ -22,35 +30,27 @@ let userInput = 'gb'
         .then(function (data) {
             console.log(data)
 
-// Will insert for loop here later, to render articles from first 5 results
-            
+            for (let i = 0; i < 5; i++) {
+                
 // Article titles, images, descriptions, links
-            console.log(data.results[0].title)
-            let title = data.results[0].title
+            let title = data.results[i].title
             
-// Image element not working yet
-            console.log(data.results[0].image_URL)
-            let img = data.results[0].image_URL
+            let img = data.results[i].image_url
 
-            console.log(data.results[0].description)
-            let description = data.results[0].description
+            let description = data.results[i].description
 
-            console.log(data.results[0].link)
-            let link = data.results[0].link
+            let link = data.results[i].link
 
+// Append to 
         let titleEl = $('<h2>').text(title)
         let imgEl = $('<img>').attr('src', img)
         let pEl = $('<p>').text(description)
-        let linkEl = $('<a>').attr('href', link).text('Link to full article')
+        let linkEl = $('<a>').attr('href', link).text('Link to full article').attr('target', 'blank')
 
         // append news information to the news section of document
         $('#news-section').append(titleEl, imgEl, pEl, linkEl)
-            
-            // }
-
-        })
+            }
+            })
+        }
 
 })
-
-
-
